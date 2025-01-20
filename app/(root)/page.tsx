375868115e8da0ceb4154b028106fb8eb242c505
daniel-kav <dankavatha01@gmail.com>
2025-01-20 20:32:37 +0300

    fix: update homepage text for clarity and engagement

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,11 +24,11 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0 ">
           <div className="flex flex-col justify-center gap-8 pl-3">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">Plan, Engage, Enjoy: Your Events, Our Expertise!</h1>
+            <p className="p-regular-20 md:p-regular-24">Discover and gain insights from 3,168+ mentors in top companies with our global network.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
-                Explore Now
+                Discover Now
               </Link>
             </Button>
           </div>
@@ -44,7 +44,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold pl-3">Trust by <br /> Thousands of Events</h2>
+        <h2 className="h2-bold pl-3">Trusted by <br /> Thousands of Event Planners</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
@@ -53,8 +53,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
 
         <Collection 
           data={events?.data}
-          emptyTitle="No Events Found"
-          emptyStateSubtext="Come back later"
+          emptyTitle="No Events Available"
+          emptyStateSubtext="Please check back later"
           collectionType="All_Events"
           limit={6}
           page={page}
