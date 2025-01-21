52ad08055bd9c18165de2c7617781c20d96e7cf8
daniel-kav <dankavatha01@gmail.com>
2025-01-21 08:46:37 +0300

    fix: update Card component to enhance hover effect and change price currency format

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -19,7 +19,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   const isEventCreator = userId === event.organizer._id.toString();
 
   return (
-    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg hover:bg-[#0c34fc1a] md:min-h-[438px]">
       <Link 
         href={`/events/${event._id}`}
         style={{backgroundImage: `url(${event.imageUrl})`}}
@@ -41,8 +41,8 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
       > 
        {!hidePrice && <div className="flex gap-2">
-          <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-            {event.isFree ? 'FREE' : `$${event.price}`}
+          <span className="p-semibold-14 w-min whitespace-nowrap rounded-full bg-green-100 px-4 py-1 text-green-60">
+            {event.isFree ? 'FREE' : `KSH ${event.price}`}
           </span>
           <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
             {event.category.name}
